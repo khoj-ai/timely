0bc476fba56fe565e5997276c1813c83b2a2b8ab
MythicalCow <raghav.tirumale@gmail.com>
2024-06-26 10:24:16 -0400

    added baseline data to the wiki_gradient_v1

--- appendwiki.py
+++ appendwiki.py
@@ -1,11 +1,11 @@
 from datasets import load_dataset
 
 ds = load_dataset("sentence-transformers/wikihow")
-with open("csv/wikidateaware_v4_mini.txt", "a") as file:
-    for i in range(1500, 3501):
+with open("csv/wiki_gradient_v1.txt", "a") as file:
+    for i in range(0, 120100):
         query = ds['train'][i]['summary']
         doc = ds['train'][i]['text']
         try:
-            file.write(f"{query}|{doc}\n")
+            file.write(f"{query}|{doc}|1\n")
         except:
             print("Error at ", i)
